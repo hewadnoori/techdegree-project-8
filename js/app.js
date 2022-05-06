5cc7a850e952e7f7fa39d643f12c5c7bd69e4b4c
Hewad Noori <hewadnoori16@gmail.com>
2022-05-06 14:18:04 -0400

    finished html and css portion

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+async function getRandomUser() {
+    const response = await fetch('https://randomuser.me/api/');
+    const data = await response.json();
+    console.log(data);
+}
+
+getRandomUser();
